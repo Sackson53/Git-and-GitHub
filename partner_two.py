40ef6af5d3d33aa934784dd91b26000e8c4516e0
Stephen Jackson <stephenj2802@gmail.com>
2023-08-02 19:55:23 -0400

    Added another equation with goodbye on it

--- partner_two.py
+++ partner_two.py
@@ -2,3 +2,14 @@ x = 4
 y = 2
 
 print((x*y+x) * y)
+
+
+'''lemme add something '''
+
+z = 10
+
+print((x*y+x) * y**z, "Goodbye")
+
+x = input("Type a letter: ")
+
+print("The letter you have typed is ", x)
